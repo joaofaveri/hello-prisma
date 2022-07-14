e3efe0a4724b88c41c77b749e432fa27fb71dafa
joaofaveri <joao.faveri@gmail.com>
2022-07-14 00:57:28 -0300

    Write data into the database

--- index.ts
+++ index.ts
@@ -5,9 +5,30 @@ const prisma = new PrismaClient
 async function main() {
     // Connect to the client
     await prisma.$connect()
+    
     // Prisma Client queries
-    const allUsers = await prisma.user.findMany()
-    console.log(allUsers);
+    // Create example users
+    await prisma.user.create({
+        data: {
+            name: 'Rich',
+            email: 'hello@prisma.com',
+            posts: {
+                create: {
+                    title: 'My first post',
+                    slug: 'my-first-post',
+                    body: 'Lots of really interesting stuff'
+                }
+            }
+        }
+    })
+
+    // Get All Users
+    const allUsers = await prisma.user.findMany({
+        include: {
+            posts: true
+        }
+    })
+    console.dir(allUsers, {depth: null})
 }
 
 main()
